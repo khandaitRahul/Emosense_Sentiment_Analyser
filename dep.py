f5a4125a7f221e24fbfd8e21ac80cdbbb10981a8
Rahul Khandait <102789478+khandaitRahul@users.noreply.github.com>
2023-12-20 03:54:06 +0530

    Add files via upload

--- dep.py
+++ dep.py
@@ -192,7 +192,7 @@ def main():
                         by tapping into customer sentiments, while individuals can enhance interpersonal connections.
                         </span>
                     """
-            link1 = " <span style='font-size:25px;letter-spacing: 0.45px;'>Ready for a memorable stay? [Book your hotel now](https://www.google.com/)</span>"
+            link1 = " <span style='font-size:25px; letter-spacing: 0.45px; margin: 30px 0px 0px 0px;'>Ready for a memorable stay? [Book your hotel now](https://www.google.com/)</span>"
             st.markdown(para1, unsafe_allow_html=True)
             st.markdown(link1, unsafe_allow_html=True)
 
@@ -204,7 +204,7 @@ def main():
 
         # __Right-col__
         with col3:
-            link2 = "<span style='font-size:25px; letter-spacing:0.45px;'>[Explore the features & benefits of this app.](https://medium.com/@pavansahu0809/predictive-modelling-for-hotel-ratings-a-data-science-approach-1d4af6431bb5 )</span>"
+            link2 = "<span style='font-size:25px; letter-spacing:0.45px; margin: 0px 0px 30px 0px;'>[Explore the features & benefits of this app.](https://medium.com/@pavansahu0809/predictive-modelling-for-hotel-ratings-a-data-science-approach-1d4af6431bb5 )</span>"
             para2 = """<span style='font-size:18px;'>
                             EmoSense is more than an app: it's a transformative tool that empowers users to navigate the web of human emotions, 
                             fostering empathy and effective communication. With EmoSense stays ahead of linguistic shifts, 
